cbeeb61d22befed557a36be0399106b47ce6a0f4
aditya12696 <100424719+aditya12696@users.noreply.github.com>
2025-01-10 21:13:57 +0530

    Day Of Week

---
+++ level3_cf/DayOfWeek.java
@@ -0,0 +1,34 @@
+class DayOfWeek{
+    public static void main(String[] args) {
+        // Check for command line arguments
+        if (args.length == 3) {
+            // Storing date
+            int month = Integer.parseInt(args[0]);
+            int date = Integer.parseInt(args[1]);
+            int year = Integer.parseInt(args[2]);
+
+            // Calculation
+            int year0 = year - (14 - month) / 12;
+            int x = year0 + (year0 / 4) - (year0 / 100) + (year0 / 400);
+            int month0 = month + 12 * ((14 - month) / 12) - 2;
+            int day = (date + x + 31 * month0 / 12) % 7;
+
+            // Result print
+            System.out.print("The day on this date is ");
+            switch (day) {
+                case 0 -> System.out.println("Sunday");
+                case 1 -> System.out.println("Monday");
+                case 2 -> System.out.println("Tuesday");
+                case 3 -> System.out.println("Wednesday");
+                case 4 -> System.out.println("Thursday");
+                case 5 -> System.out.println("Friday");
+                case 6 -> System.out.println("Saturday");
+                default -> System.out.println("Something went wrong");
+            }
+        }
+		else{
+			System.out.println("No command line argument found");
+		}
+        
+    }
+}
