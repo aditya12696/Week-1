2f74371f27cd46c7a974fa96eeb2bd43f27a2b2d
aditya12696 <100424719+aditya12696@users.noreply.github.com>
2025-01-10 22:59:36 +0530

    Voting Age

---
+++ level1_array/VotingAge.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class VotingAge {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+		// Arrays to store ages
+        int[] ages = new int[10];
+        
+        // Taking input for the ages of students
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Enter age of student " + (i + 1) + ": ");
+            ages[i] = sc.nextInt();
+        }
+
+        // Checking voting eligibility based on age
+        for (int i = 0; i < ages.length; i++) {
+            if (ages[i] < 0) {
+                System.out.println("Invalid age entered for student " + (i + 1));
+            } else if (ages[i] >= 18) {
+                System.out.println("The student with the age " + ages[i] + " can vote.");
+            } else {
+                System.out.println("The student with the age " + ages[i] + " cannot vote.");
+            }
+        }
+
+        sc.close();
+    }
+}
