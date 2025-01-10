507eeac4f1a614a2870ccba7ed96303c9e8e40db
aditya12696 <100424719+aditya12696@users.noreply.github.com>
2025-01-10 16:11:04 +0530

    Find Age Of Harry

---
+++ Day1/Level_1/FindAgeOfHarry.java
@@ -0,0 +1,16 @@
+class FindAgeOfHarry {
+	// Write a program to find the age of Harry
+	public static void main(String[] args) {
+		// Current year
+		int currentYear = 2024;
+
+		// Birth year of Harry
+		int birthYear = 2000;
+
+		// Current age of Harry
+		int harryAge = (currentYear - birthYear);
+		
+		// Display harry age
+		System.out.println("Harry's age in 2024 is " + harryAge);
+	}
+}
