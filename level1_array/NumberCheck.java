ade4fc4a9dc8f29974e8e8f204ac0a85272f0350
aditya12696 <100424719+aditya12696@users.noreply.github.com>
2025-01-10 22:58:18 +0530

    Number Check

---
+++ level1_array/NumberCheck.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class NumberCheck {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+		
+		// Arrays to store number
+        int[] numbers = new int[5];
+
+        // Taking input for the numbers
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print("Enter number " + (i + 1) + ": ");
+            numbers[i] = sc.nextInt();
+        }
+
+        // Checking if numbers are positive, negative, or zero
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] > 0) {
+                if (numbers[i] % 2 == 0) {
+                    System.out.println("The number " + numbers[i] + " is positive and even.");
+                } else {
+                    System.out.println("The number " + numbers[i] + " is positive and odd.");
+                }
+            } else if (numbers[i] < 0) {
+                System.out.println("The number " + numbers[i] + " is negative.");
+            } else {
+                System.out.println("The number " + numbers[i] + " is zero.");
+            }
+        }
+
+        // Comparing first and last element
+        if (numbers[0] == numbers[numbers.length - 1]) {
+            System.out.println("First and last elements are equal.");
+        } else if (numbers[0] > numbers[numbers.length - 1]) {
+            System.out.println("First element is greater than last element.");
+        } else {
+            System.out.println("First element is less than last element.");
+        }
+
+        sc.close();
+    }
+}
