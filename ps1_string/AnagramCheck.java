f959746e8b8257019ad4f17292a7f7818e57a81a
aditya12696 <100424719+aditya12696@users.noreply.github.com>
2025-01-14 10:43:53 +0530

    Anagram Check

---
+++ ps1_string/AnagramCheck.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class AnagramCheck {
+
+    // Function to check if two strings are anagrams
+    private static boolean areAnagrams(String str1, String str2) {
+        if (str1.length() != str2.length()) {
+            return false;
+        }
+        char[] charArray1 = str1.toCharArray();
+        char[] charArray2 = str2.toCharArray();
+        Arrays.sort(charArray1);
+        Arrays.sort(charArray2);
+        return Arrays.equals(charArray1, charArray2);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter string 1: ");
+        String str1 = scanner.nextLine();
+        System.out.print("Enter string 2: ");
+        String str2 = scanner.nextLine();
+
+        if (areAnagrams(str1, str2)) {
+            System.out.println("The strings are anagrams.");
+        } else {
+            System.out.println("The strings are not anagrams.");
+        }
+        scanner.close();
+    }
+}
